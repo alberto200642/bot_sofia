e77881edcff092f6e3f1535593c3b88f416d88e4
Alberto Azevedo <41203277-alberto200642@users.noreply.replit.com>
2025-04-26 01:35:21 +0000

    Inclui set_webhook.py e importa no main

--- set_webhook.py
+++ set_webhook.py
@@ -6,3 +6,5 @@ WEBHOOK_URL = "https://bot-sofia.onrender.com/7634899396:AAHMYtF01bJfVjAK36ASmu6
 bot = telebot.TeleBot(TOKEN)
 bot.remove_webhook()
 bot.set_webhook(url=WEBHOOK_URL)
+
+print("✅ Webhook configurado com sucesso.")
